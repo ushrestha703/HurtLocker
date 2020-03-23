bcd0ebabdd992cad2e5fe8e16b4d21961a04b55f
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-23 13:04:30 -0400

    Working on it.

---
+++ src/main/java/JerksonDataParser.java
@@ -0,0 +1,7 @@
+public class JerksonDataParser {
+
+    //TODO
+    // Output a string formatted list that resembles (outputFormat.txt)
+    // Use Pattern and Matcher
+    // Be able to handle Exceptions
+}
---
+++ src/test/java/TestJerksonDataParser.java
@@ -0,0 +1,8 @@
+public class TestJerksonDataParser {
+
+    //TODO
+    // Test for Type that have key but no Value
+    // Test for Exceptions
+    // Test for sample Pattern and Matcher
+
+}
